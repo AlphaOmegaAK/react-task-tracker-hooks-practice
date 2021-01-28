5246eddcb972b63616924e8099a8553c85d67c6f
alpha0mgaak <alphaunionxt@gmail.com>
2021-01-28 10:31:36 -0500

    Example of Global State - Passing from App down to Tasks Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useState } from 'react';
 import Header from './Components/Header';
 import Tasks from './Components/Tasks'
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 0,
+      text: "Doctors Appointment",
+      day: "Jan 19 at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 1,
+      text: "Take Child to School",
+      day: "Jan 20 at 8:30am",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Buy Milk",
+      day: "Jan 21 at 4:00pm",
+      reminder: false,
+    }
+  ]); // GLOBAL State
+
   return (
     <div className="container">
       <Header title="Task Tracker"/> 
-      <Tasks/>
+      <Tasks tasks={tasks}/>
 
     
     </div>
--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,51 +1,7 @@
-import { useState } from 'react';
-
-// ----- Hard Coded pre-use of state ---------
-// Don't want to hardcode -> should be in STATE:dynamic
-// const tasks = [
-//   {
-//     id: 1,
-//     text: "Doctors Appointment",
-//     day: "Jan 20 at 1:30pm",
-//     reminder: true,
-//   },
-//   {
-//     id: 2,
-//     text: "Take Child to School",
-//     day: "Jan 21 at 8:30am",
-//     reminder: true,
-//   },
-//   {
-//     id: 3,
-//     text: "Buy Milk",
-//     day: "Jan 22 at 4:00pm",
-//     reminder: false,
-//   }
-// ];
-// --------------------------------------------
 
 
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Jan 20 at 1:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Take Child to School",
-    day: "Jan 21 at 8:30am",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Buy Milk",
-    day: "Jan 22 at 4:00pm",
-    reminder: false,
-  }
-])
+const Tasks = ({ tasks }) => {
+
   return (
     <>
     {/* Create a List using the map() method */}
