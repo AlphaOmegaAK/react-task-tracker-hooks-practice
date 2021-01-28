834863d84c0d224c70093c177308afc406187d3e
alpha0mgaak <alphaunionxt@gmail.com>
2021-01-28 10:21:32 -0500

    use State example

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+
+// ----- Hard Coded pre-use of state ---------
 // Don't want to hardcode -> should be in STATE:dynamic
-const tasks = [
+// const tasks = [
+//   {
+//     id: 1,
+//     text: "Doctors Appointment",
+//     day: "Jan 20 at 1:30pm",
+//     reminder: true,
+//   },
+//   {
+//     id: 2,
+//     text: "Take Child to School",
+//     day: "Jan 21 at 8:30am",
+//     reminder: true,
+//   },
+//   {
+//     id: 3,
+//     text: "Buy Milk",
+//     day: "Jan 22 at 4:00pm",
+//     reminder: false,
+//   }
+// ];
+// --------------------------------------------
+
+
+const Tasks = () => {
+  const [tasks, setTasks] = useState([
   {
     id: 1,
     text: "Doctors Appointment",
@@ -18,11 +45,7 @@ const tasks = [
     day: "Jan 22 at 4:00pm",
     reminder: false,
   }
-];
-
-
-
-const Tasks = () => {
+])
   return (
     <>
     {/* Create a List using the map() method */}
@@ -35,3 +58,10 @@ const Tasks = () => {
 
 
 export default Tasks;
+
+// State is immutable, it gets re-created and sent back down to component ( it's one way direction data )
+// To update the state you would set the state : setTasks([...tasks], {new object aka task}) (in this case)
+// i.e : can't push()/pop() to/from state
+
+// Prop Drilling : to have global state place it at highest level component - App.js
+// or use Context, Redux to have a 'cloud' that you can pull state from also globally
